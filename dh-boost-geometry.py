34b6ee9223617f4bf6cc36a498207d504f429c25
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2015-03-09 20:12:15 +0100

    Improve turn_info dumper
    Display operations in value field.
    After expansion display operations directly, ommit boost:array.

--- dh-boost-geometry.py
+++ dh-boost-geometry.py
@@ -232,11 +232,14 @@ def qdump__boost__geometry__detail__overlay__turn_info(d, value):
     selectable_start = value["selectable_start"]
     operations = value["operations"]
     simple_method = str(method)[41:]
+    simple_op0 = str(operations["elems"][0]["operation"])[44:]
+    simple_op1 = str(operations["elems"][1]["operation"])[44:]
+    simple_operations = simple_op0 + ", " + simple_op1;
 
     val_str = simple_method
     dim = boost__geometry__point_dimension(d, point.type)
     if dim > 0:
-        val_str += " " + boost__geometry__point_array_to_text(point["m_values"], dim)
+        val_str += " (" + simple_operations + ") " + boost__geometry__point_array_to_text(point["m_values"], dim)
     d.putValue(val_str)
 
     d.putNumChild(5)
@@ -249,7 +252,14 @@ def qdump__boost__geometry__detail__overlay__turn_info(d, value):
                 d.putNumChild(0)
             d.putSubItem("discarded", discarded)
             d.putSubItem("selectable_start", selectable_start)
-            d.putSubItem("operations", operations)
+            with SubItem(d, "operations"):
+                d.putValue(simple_operations)
+                d.putType(operations.type)
+                d.putNumChild(2)
+                if d.isExpanded():
+                    with Children(d, 2):
+                        d.putSubItem("[0]", operations["elems"][0])
+                        d.putSubItem("[1]", operations["elems"][1])
 
 def qdump__boost__geometry__side_info(d, value):
     sides = value["sides"]
